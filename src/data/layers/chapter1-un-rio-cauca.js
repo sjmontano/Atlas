65a8f1c357d18b17028d26e289691786e3c1d975
sjmontano <smontano@unimayor.edu.co>
2026-08-12 00:00:37 -0500

    fix(calibration): actualizar PGW y dimensiones de las capas de chapter1-un-rio-cauca tras recalibrar

--- src/data/layers/chapter1-un-rio-cauca.js
+++ src/data/layers/chapter1-un-rio-cauca.js
@@ -1,7 +1,7 @@
 const URIO_CAUCA_BASE = '/assets/maps/capas/un-rio-cauca'
-const URIO_CAUCA_PGW = [0, 0.001232510189, 0.0012309569997728162, 0, -79.24025330897976, -0.523978312770837]
-const URIO_CAUCA_W = 5778
-const URIO_CAUCA_H = 10285
+const URIO_CAUCA_PGW = [0, 0.001232510189, 0.0012309569997728162, 0, -79.27447278696201, -0.6228009147673822]
+const URIO_CAUCA_W = 5836
+const URIO_CAUCA_H = 10388
 
 const urioLayer = (id, name, group, order, opacity = 0.8) => ({
   id: `urc-${id}`,
