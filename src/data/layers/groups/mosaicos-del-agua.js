b8668e04478bf8c474bf2e378bbb38a1e7812862
sjmontano <smontano@unimayor.edu.co>
2026-08-11 22:53:56 -0500

    feat(layers): añadir 7 capas de agua interactivas para chapter1-mosaicos-del-agua

---
+++ src/data/layers/groups/mosaicos-del-agua.js
@@ -0,0 +1,7 @@
+const g = (id, name, order) => ({ id, name, order })
+
+export const WATER_GROUPS = [
+  g('mda-agua', 'El agua de arriba a abajo', 1),
+  g('mda-acuiferos', 'Acuíferos', 2),
+  g('mda-flujos', 'Flujos y zonas del acuífero', 3),
+]
--- src/data/layers/index.js
+++ src/data/layers/index.js
@@ -1,12 +1,16 @@
 import { CHAPTER1_ECOSYSTEMS_LAYERS } from './chapter1-ecosistemas.js'
 import { ECOSYSTEMS_GROUPS } from './groups/ecosistemas.js'
+import { CHAPTER1_WATER_LAYERS } from './chapter1-mosaicos-del-agua.js'
+import { WATER_GROUPS } from './groups/mosaicos-del-agua.js'
 
 export const LAYERS = {
   'chapter1-ecosistemas': CHAPTER1_ECOSYSTEMS_LAYERS,
+  'chapter1-mosaicos-del-agua': CHAPTER1_WATER_LAYERS,
 }
 
 export const LAYER_GROUPS = {
   'chapter1-ecosistemas': ECOSYSTEMS_GROUPS,
+  'chapter1-mosaicos-del-agua': WATER_GROUPS,
 }
 
 export function getMapLayers(mapId) {
